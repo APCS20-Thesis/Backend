8b683f178aba3b1b7f0da66032b30308c444b019
Yếnn <vanhoangyen0206@gmail.com>
2024-01-21 21:11:07 +0700

    Add proto validation package

--- protoc.sh
+++ protoc.sh
@@ -1,4 +1,4 @@
-protoc -I ./third_party/googleapis -I ./api \
+protoc -I ./third_party/googleapis -I ./third_party/envoyproxy -I ./api \
 --proto_path=./api --go_out=api --go_opt=paths=source_relative \
 --go-grpc_out=api --go-grpc_opt=paths=source_relative \
 --grpc-gateway_out=api --grpc-gateway_opt=paths=source_relative \
