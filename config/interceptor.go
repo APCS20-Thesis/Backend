251ee50efa0b13946c8e1474a97d22b03d92ebfe
Yếnn <vanhoangyen0206@gmail.com>
2024-02-02 21:34:44 +0700

    Remove api CreateDataSourceMySQL

--- config/interceptor.go
+++ config/interceptor.go
@@ -3,8 +3,7 @@ package config
 func AccessibleRoles() map[string][]string {
 	const rootServicePath = "/api.CDPService/"
 	return map[string][]string{
-		rootServicePath + "Admin":                 {"admin"},
-		rootServicePath + "GetAccountInfo":        {"admin", "user"},
-		rootServicePath + "CreateDataSourceMySQL": {"admin", "user"},
+		rootServicePath + "Admin":          {"admin"},
+		rootServicePath + "GetAccountInfo": {"admin", "user"},
 	}
 }
