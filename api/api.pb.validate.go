a6053440b5ea8c4df91ab579a1b18b58c0fbeb12
Yếnn <vanhoangyen0206@gmail.com>
2024-08-05 10:56:03 +0700

    Rebase proto

--- api/api.pb.validate.go
+++ api/api.pb.validate.go
@@ -6811,6 +6811,8 @@ func (m *GetMasterSegmentProfileRequest) Validate() error {
 
 	// no validation rules for Id
 
+	// no validation rules for CdpSystemUuid
+
 	return nil
 }
 
@@ -7401,6 +7403,150 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = GetDataRunsProportionResponseValidationError{}
+
+// Validate checks the field values on GetBehaviorProfileRequest with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, an error is returned.
+func (m *GetBehaviorProfileRequest) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for Id
+
+	// no validation rules for ForeignKeyValue
+
+	return nil
+}
+
+// GetBehaviorProfileRequestValidationError is the validation error returned by
+// GetBehaviorProfileRequest.Validate if the designated constraints aren't met.
+type GetBehaviorProfileRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e GetBehaviorProfileRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e GetBehaviorProfileRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e GetBehaviorProfileRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e GetBehaviorProfileRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e GetBehaviorProfileRequestValidationError) ErrorName() string {
+	return "GetBehaviorProfileRequestValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e GetBehaviorProfileRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sGetBehaviorProfileRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = GetBehaviorProfileRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = GetBehaviorProfileRequestValidationError{}
+
+// Validate checks the field values on GetBehaviorProfileResponse with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, an error is returned.
+func (m *GetBehaviorProfileResponse) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for Code
+
+	// no validation rules for Message
+
+	// no validation rules for Count
+
+	return nil
+}
+
+// GetBehaviorProfileResponseValidationError is the validation error returned
+// by GetBehaviorProfileResponse.Validate if the designated constraints aren't met.
+type GetBehaviorProfileResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e GetBehaviorProfileResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e GetBehaviorProfileResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e GetBehaviorProfileResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e GetBehaviorProfileResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e GetBehaviorProfileResponseValidationError) ErrorName() string {
+	return "GetBehaviorProfileResponseValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e GetBehaviorProfileResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sGetBehaviorProfileResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = GetBehaviorProfileResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = GetBehaviorProfileResponseValidationError{}
 
 // Validate checks the field values on GetListDataSourcesResponse_DataSource
 // with the rules defined in the proto definition for this message. If any
